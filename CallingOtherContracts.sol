6013b14373ccdade22b8073f82f82fd683ab97f0
Akshayachu123 <37238867+Akshayachu123@users.noreply.github.com>
2020-09-03 13:23:16 +0530

    two ways of calling other contracts

---
+++ CallingOtherContracts.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.6.0;
+
+contract Callee{
+    uint public x;
+    uint public value;
+    
+    function setX(uint _x) public returns(uint){
+        x = _x;
+        return x;
+    }
+    
+    function setXandSendEther(uint _x) public payable returns(uint,uint){
+        x = _x;
+        value = msg.value;
+        
+        return(x,value);
+    }
+}
+
+contract Caller{
+    
+    function setX(Callee _callee ,uint _x) public returns(uint){
+        uint x=_callee.setX(_x);
+    }
+    
+    function setFromAddress(address _addr,uint _x) public{
+        Callee callee= Callee(_addr);
+        callee.setX(_x);
+    }
+    
+    function setXandSendEther(Callee _callee,uint _x) public payable{
+        (uint x,uint value) = _callee.setXandSendEther.value(msg.value)(_x);
+    }
+}
