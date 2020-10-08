714da7597fab03248314a1c9c66ab35310043fac
Akshaya G R <37238867+akshayacode@users.noreply.github.com>
2020-10-08 19:35:44 +0530

    to check isFriendsCricle

--- Circle.sol
+++ Circle.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.6.0;
+pragma solidity ^0.7.0;
 
 contract Circles{
     
@@ -7,15 +7,40 @@ contract Circles{
         address waddress;
         int balance;
     }
+    
+    struct Loan {
+        
+        uint loanamount;
+        bool agree;
+        mapping(address => bool) agreements; 
+    }
+
 
     /// This declares a state variable that stores a `FriendsCircle` struct for each possible address.
     mapping(address => FriendsCircle) public circle;
+    
+    Loan[] public loans;
+
  
     function createFriendsCircle(string memory _name, address _waddress) public {
         
         require(_waddress != circle[_waddress].waddress); //only one address per person
-        FriendsCircle memory FriendsCircle = FriendsCircle({name: _name, waddress: _waddress, balance: 0});
-        circle[_waddress] = FriendsCircle;
+        FriendsCircle memory friendscircle = FriendsCircle({name: _name, waddress: _waddress, balance: 0});
+        circle[_waddress] = friendscircle;
     }
+    
+   
+    function isFriendCircle(address _waddress) public view returns (bool) {
+        if (_waddress == circle[_waddress].waddress) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+        
+        
 }
+    
+   
+
 
