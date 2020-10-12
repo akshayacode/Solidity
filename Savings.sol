106979b88324a2b9005689f2ff4dbaf645ba4064
Akshaya G R <37238867+akshayacode@users.noreply.github.com>
2020-10-12 12:41:36 +0530

    Participants can join fund

--- Savings.sol
+++ Savings.sol
@@ -1,21 +1,42 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.7.0;
 
 contract Savings{
     
-    struct Fund{
-            string  fundName;
-            uint  installmentAmount;
-            uint  noOfInstallments;
-            uint  noOfParticipants;
-            address manager;
-    }
+    string public fundName;
+    uint public installmentAmount;
+    uint public noOfInstallments;
+    uint public noOfParticipants;
+  
     
-    mapping(address => Fund) public ChitFund;
     uint public numFunds;
     
+    address public manager;
+    mapping(address => bool) public participants;
+    uint public noOfParticipantsJoined;
+    uint public fundBalance;
+    bool public status = true;
     
-    function createFund(string memory name, uint amount, uint installments, uint participants) public {
-        ChitFund[msg.sender] = Fund(name, amount, installments, participants, msg.sender);
-        numFunds++;
+    address[] public participantsArray;
+    address[] public participantsToBePaid;
+    
+    function createFund(string memory name, uint amount, uint installments, uint _Participants) public {
+       fundName = name;
+       installmentAmount = amount;
+       noOfInstallments = installments;
+       noOfParticipants = _Participants;
+       numFunds++;
+    }
+    
+    function joinFund() public {
+        require(noOfParticipantsJoined < noOfParticipants);
+        require(participants[msg.sender] != true);
+        require(status);
+
+        // participants.push(msg.sender);
+        participants[msg.sender] = true;
+        participantsArray.push(msg.sender);
+        participantsToBePaid.push(msg.sender);
+        noOfParticipantsJoined++;
     }
 }
