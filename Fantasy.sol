2355395ab43acd33a7918a62e15d93219f81252c
Akshaya G R <37238867+akshayacode@users.noreply.github.com>
2020-10-15 17:05:53 +0530

    add player details

--- Fantasy.sol
+++ Fantasy.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.7.0;
+pragma experimental ABIEncoderV2;
 
 contract Fantasy {
     
@@ -18,15 +19,38 @@ contract Fantasy {
     
     mapping(uint => contest) public Contestdetails;
     
+    struct Player{
+        string name;
+        string category;
+        uint value;
+        
+    }
+    
+    struct Teams{
+        string Teamname;
+        string[] playername;
+    }
+    
+    Teams[] public teamData;
+    
+    mapping(uint => Player) public playerdetails;
+    uint players;
+    
+    
+    
      function createcontest(string memory name,uint entryfee,uint team_size,uint prizeamount,uint spots) public {
         Contestdetails[numcontest] = contest(name,entryfee,team_size,prizeamount,spots);
     }
     
-    function createTeam(string memory teamname) public{
-         teams.push(teamname);
+    function CreateTeam(string memory _name, string[] memory _players) public {
+        teamData.push(Teams(_name, _players));
     }
     
-    function addPlayers(string memory name) public {
-        
+    function addPlayers(uint _index ,string memory _player,string memory category,uint value) public {
+        teamData[_index].playername.push(_player);
+        playerdetails[players] = Player(_player,category,value);
+    }
+    function getPlayersname(uint256 _index) public view returns (string[] memory) {
+        return teamData[_index].playername;
     }
 }
