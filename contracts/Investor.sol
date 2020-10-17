5841e7f236b039da14d781b8499edc472a2118c1
Akshaya G R <37238867+akshayacode@users.noreply.github.com>
2020-10-17 10:07:28 +0530

    investor deposits FD

--- contracts/Investor.sol
+++ contracts/Investor.sol
@@ -1,17 +1,18 @@
 //SPDX-License-Identifier: MIT;
 pragma solidity ^0.7.0;
 
+import './Accounts.sol';
 contract InvestorContract {
    
-
-    mapping (address => Investor) public investors; // Investor public key => Investor
+    
+    mapping (address => Investor) public investors;
 
    
-    uint numLoans;
-    uint locker;
+   
+    address locker = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
 
     
-    mapping (uint => Loan) public loans;
+   
 
     mapping(address => bool) hasOngoingInvestment;
 
@@ -23,24 +24,6 @@ contract InvestorContract {
     }
    
 
-    struct Loan{
-
-        //For traversal and indexing
-        bool openLoan;
-        uint loanId;
-
-        address borrower;
-        address investor;
-        uint interest_rate;
-        uint duration;
-        uint principal_amount;
-        uint original_amount;
-        uint amount_paid;
-        uint startTime;
-        uint monthlyCheckpoint;
-        uint appId;
-
-    }
  
    
     function createInvestor(string memory name) public {
@@ -50,67 +33,21 @@ contract InvestorContract {
         investor.EXISTS = true;
         //require (borrowers[msg.sender].EXISTS != true);
         investors[msg.sender] = investor;
-        // investorArray[investorArray.length] = investor.investor_public_key;
         hasOngoingInvestment[msg.sender] = false;
        // balances[msg.sender] = 0; // Init balance
 
     }
-    
 
-    
-    // function grantLoan(uint appId) public {
-    //     //Check sufficient balance
-    //     require(balances[msg.sender] >= applications[appId].credit_amount);
-    //     require(hasOngoingInvestment[msg.sender] == false);
+    function DepositFD(address contractaddr,uint amount,address _addr) public payable {
+        //Check sufficient balance
+        Accounts acc = Accounts(contractaddr);
+        require(acc.viewBalance(_addr) >= amount);
+        require(hasOngoingInvestment[msg.sender] == false);
 
-    //     // Take from sender and give to reciever
-    //     balances[msg.sender] -= applications[appId].credit_amount;
-    //     locker +=  applications[appId].credit_amount;
+        acc.transfer(_addr,locker,amount);
         
-    //     hasOngoingInvestment[msg.sender] = true;
+        hasOngoingInvestment[msg.sender] = true;
 
-
-    // }
-
-    
-    // function ifApplicationOpen(uint index) public view returns (bool){
-    //     LoanApplication storage app = applications[index];
-    //     if(app.openApp) return true; else return false;
-    // }
-    function ifLoanOpen(uint index) public view returns (bool){
-        Loan storage loan = loans[index];
-        if (loan.openLoan == true) return true; else return false;
     }
-    // function getApplicationData(uint index) public view returns (uint[] memory, string memory, address){
-    //     string storage otherData = applications[index].otherData;
-    //     uint[] memory numericalData = new uint[](4);
-    //     numericalData[0] = index;
-    //     numericalData[1] = applications[index].duration;
-    //     numericalData[2] = applications[index].credit_amount;
-    //     numericalData[3] = applications[index].interest_rate;
 
-    //     address borrower = applications[index].borrower;
-    //     return (numericalData, otherData, borrower);
-    //     // numericalData format = [index, duration, amount, interestrate]
-    // }
-    function getLoanData(uint index) public view returns (uint[] memory, address, address){
-        uint[] memory numericalData = new uint[](9);
-        numericalData[0] = index;
-        numericalData[1] = loans[index].interest_rate;
-        numericalData[2] = loans[index].duration;
-        numericalData[3] = loans[index].principal_amount;
-        numericalData[4] = loans[index].original_amount;
-        numericalData[5] = loans[index].amount_paid;
-        numericalData[6] = loans[index].startTime;
-        numericalData[7] = loans[index].monthlyCheckpoint;
-        numericalData[8] = loans[index].appId;
-
-        return (numericalData, loans[index].borrower, loans[index].investor);
-        // numericalData format = [index, interestrate, duration, p_amnt, o_amnt, paid_amnt, starttime, app_index]
-    }
-    // function getNumApplications() public view returns  (uint) { return numApplications;}
-    // function getNumLoans() public view returns (uint) { return numLoans;}
-    // function isInvestor(address account) public view returns (bool) {return investors[account].EXISTS;}
-    // function isBorrower(address account) public view returns (bool) {return borrowers[account].EXISTS;}
-    // function getTime() public  view returns (uint){return block.timestamp;}
 }
