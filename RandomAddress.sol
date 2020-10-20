c0464f19c5e0e9ce053b09b148c62b78c8164a24
Akshaya G R <37238867+akshayacode@users.noreply.github.com>
2020-10-20 18:19:18 +0530

    generating Random Address

--- RandomAddress.sol
+++ RandomAddress.sol
@@ -1,10 +1,13 @@
-pragma solidity ^0.4.25;
+pragma solidity ^0.7.0;
 
 contract Random{
     
    function uinqueaddress() public view returns(address){
        
-       address addr = address(keccak256(abi.encodePacked(now)));
+       //address addr = address(keccak256(abi.encodePacked(now))); (for sol 0.4.25)
+       
+       address addr = address(uint160(uint(keccak256(abi.encodePacked(block.timestamp)))));
+
         return address(addr);
    } 
 
