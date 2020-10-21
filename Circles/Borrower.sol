6b82928ca5e6f6e645ef60624888e55e05bef7fb
Akshaya G R <37238867+akshayacode@users.noreply.github.com>
2020-10-21 12:48:53 +0530

    initiate circle Limit

--- Circles/Borrower.sol
+++ Circles/Borrower.sol
@@ -14,20 +14,31 @@ contract BorrowerContract {
         address[] defaultAddress;
     }
     
-    mapping(uint => Borrower) public BorrowerCircle;
+    mapping(address => Borrower) public BorrowerCircle;
     
     function setAccountContract(address Caddr) public {
         AccountsContract = Caddr;
     }
-    
-    function createCircle(string memory name,uint index) public {
-        BorrowerCircle[index].borrower_public_key = msg.sender;
-        BorrowerCircle[index].circles.push(name);
-        BorrowerCircle[index].defaultAddress.push(address(uint160(uint(keccak256(abi.encodePacked(block.timestamp))))));
+    function createBorrower() public {
+        BorrowerCircle[msg.sender].borrower_public_key = msg.sender;
+    }
+    function createCircle(string memory name) public {
+        
+        BorrowerCircle[msg.sender].circles.push(name);
+        BorrowerCircle[msg.sender].defaultAddress.push(address(uint160(uint(keccak256(abi.encodePacked(block.timestamp))))));
        
     }
     
-    function getdefaultAddress(uint index) public view returns(address[] memory) {
-        return BorrowerCircle[index].defaultAddress;
+    function getdefaultAddress(uint index) public view returns(address) {
+        return BorrowerCircle[msg.sender].defaultAddress[index];
     }
+    
+    function initiatecircleLimit(uint index,uint circleLimit) public
+    {
+     //BorrowerCircle[msg.sender].circles[index]
+     address getter = BorrowerCircle[msg.sender].defaultAddress[index];
+     Accounts acc = Accounts(AccountsContract);
+     acc.transfer(msg.sender,getter,circleLimit);
+    }
+    
 }
