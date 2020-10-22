926f1d9a0e38d2ac0001f4fb7a7dd94bb07c8c4f
Akshaya G R <37238867+akshayacode@users.noreply.github.com>
2020-10-22 13:50:13 +0530

    View Application

--- Circles/Borrower.sol
+++ Circles/Borrower.sol
@@ -8,6 +8,7 @@ contract CirclesContract {
     struct Circle{
         string name;
         address manager;
+        address borrower;
         address locker;
         address[] participants;
         uint circleLimit;
@@ -20,9 +21,29 @@ contract CirclesContract {
     
     mapping(address => Borrower) public borrwers;
     
+    enum Status{
+        Created,
+        Progress,
+        Shortlisted,
+        Accepted
+    }
+    struct LoanApplication {
+        //uint id;
+        uint duration;
+        uint interest_rate;
+        uint credit_amount;
+        uint total_circle_limit;
+        //uint available_limit;
+        uint EMI;
+        Status status;
+        
+    }
+    mapping(uint => LoanApplication) public applications;
+    uint numapplications;
     function setAccountsContract(address addr) public {
         AccountsContract = addr;
     }
+    
     
     function createCircle(uint id,string memory name,uint circleLimit) public{
         borrwers[msg.sender].borrower = msg.sender;
@@ -37,11 +58,39 @@ contract CirclesContract {
     
     
     function viewCircle(address addr,uint id) public view returns(string memory,address,address[] memory,uint) {
-        return(borrwers[addr].circles[id].name,borrwers[msg.sender].circles[id].manager,borrwers[msg.sender].circles[id].participants,borrwers[msg.sender].circles[id].circleLimit);
+        return(borrwers[addr].circles[id].name,
+               borrwers[msg.sender].circles[id].manager,
+               borrwers[msg.sender].circles[id].participants,
+               borrwers[msg.sender].circles[id].circleLimit);
     }
     
     function joincircle(address addr,uint id) public {
+        Accounts acc = Accounts(AccountsContract);
+        acc.transfer(msg.sender,borrwers[addr].circles[id].locker,borrwers[addr].circles[id].circleLimit);
         borrwers[addr].circles[id].participants.push(msg.sender);
     }
     
+    function CreateApplication(uint duration,uint interest_rate,uint credit_amount,uint total_circle_limit,uint EMI) public {
+        applications[numapplications] = LoanApplication(duration,interest_rate,credit_amount,total_circle_limit,EMI,Status.Created);
+    }
+    
+    function viewApplication(uint id) public view returns(uint,uint,uint,uint,uint,Status)
+    {
+        return (applications[id].duration,
+                applications[id].interest_rate,
+                applications[id].credit_amount,
+                applications[id].total_circle_limit,
+                applications[id].EMI,
+                applications[id].status);
+        
+        //return applications[id]; (not working)
+    }
+    
+    function agreeForLoan(uint id) public {
+        // code 
+    }
+    
+    function ifAgreed() public {
+        
+    }
 }
