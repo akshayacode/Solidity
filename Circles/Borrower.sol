d187f1e3150ef24a24c5019f8aeb4bd4645b542e
Hema kumar <hemakumarm72@gmail.com>
2020-10-23 14:32:40 +0530

    roll back

--- Circles/Borrower.sol
+++ Circles/Borrower.sol
@@ -2,6 +2,12 @@
 pragma solidity ^0.7.0;
 
 import './Accounts.sol';
+   enum Status{
+        Created,
+        Progress,
+        Shortlisted,
+        Approved
+    }
 contract CirclesContract {
     
     address public AccountsContract;
@@ -12,19 +18,9 @@ contract CirclesContract {
         address locker;
         address[] participants;
         uint circleLimit;
-      mapping (address => participantsteams) participantslist;
+        address investor;
         
     }
-     mapping (address => participantsteams) public participantslist;
-     
-        
-    
-    struct participantsteams{
-        uint id;
-        address participantsno;
-        bool agree;
-    }
-    
     
     struct Borrower{
         address borrower;
@@ -33,12 +29,7 @@ contract CirclesContract {
     
     mapping(address => Borrower) public borrwers;
     
-    enum Status{
-        Created,
-        Progress,
-        Shortlisted,
-        Accepted
-    }
+ 
     struct LoanApplication {
         //uint id;
         uint duration;
@@ -52,6 +43,28 @@ contract CirclesContract {
     }
     mapping(uint => LoanApplication) public applications;
     uint numapplications;
+    
+    mapping(address => bool) hasOngoingLoan;
+    mapping(address => bool) hasOngoingApplication;
+    
+    struct Loan{
+        bool openLoan;
+        uint loanId;
+        address borrower;
+        address investor;
+        uint interest_rate;
+        uint duration;
+        uint principal_amount;
+        uint original_amount;
+        uint amount_paid;
+        uint startTime;
+        uint monthlyCheckpoint;
+        uint appId;
+
+    }
+    mapping(uint => Loan) public loans;
+    uint numLoans;
+    
     function setAccountsContract(address addr) public {
         AccountsContract = addr;
     }
@@ -69,73 +82,91 @@ contract CirclesContract {
     }
     
     
-    function viewCircle(address addr,uint id) public view returns(string memory,address,address[] memory,uint, address) {
+    function viewCircle(address addr,uint id) public view returns(string memory,address,address[] memory,uint) {
         return(borrwers[addr].circles[id].name,
-               borrwers[addr].circles[id].manager,
-               borrwers[addr].circles[id].participants,
-               borrwers[addr].circles[id].circleLimit,
-               borrwers[addr].circles[id].participantslist[addr].participantsno);
+               borrwers[msg.sender].circles[id].manager,
+               borrwers[msg.sender].circles[id].participants,
+               borrwers[msg.sender].circles[id].circleLimit);
     }
     
     function joincircle(address addr,uint id) public {
         Accounts acc = Accounts(AccountsContract);
-        
         acc.transfer(msg.sender,borrwers[addr].circles[id].locker,borrwers[addr].circles[id].circleLimit);
         borrwers[addr].circles[id].participants.push(msg.sender);
-      
-         uint a = borrwers[addr].circles[id].participants.length;
-        borrwers[addr].circles[id].participantslist[msg.sender].id = a + 1;
-        borrwers[addr].circles[id].participantslist[msg.sender].participantsno = msg.sender;
-        borrwers[addr].circles[id].participantslist[msg.sender].agree = false;
-        
-    }
-    function getdata(address addr, uint id) public payable{
-      
-        
-        for (uint m = 0; m <  borrwers[addr].circles[id].participants.length; m++) {
-             borrwers[msg.sender].circles[id].participants[m];
-             
-        }
-
-        
     }
     
-    function getcirclelength(address addr, uint id) public view returns(uint){
-        
-      return borrwers[addr].circles[id].participants.length;
-    }
-    
-    function CreateApplication(uint duration,uint interest_rate,uint credit_amount,uint total_circle_limit,uint EMI) public {
+    function CreateApplication(address addr,uint id,uint duration,uint interest_rate,uint credit_amount,uint total_circle_limit,uint EMI) public {
+        borrwers[addr].circles[id].borrower = msg.sender;
         applications[numapplications] = LoanApplication(duration,interest_rate,credit_amount,total_circle_limit,EMI,Status.Created);
     }
     
-    function viewApplication(uint id) public view returns(uint,uint,uint,uint,uint,Status)
+    function viewApplication(uint id) public view returns(uint,uint,uint,uint,uint)
     {
         return (applications[id].duration,
                 applications[id].interest_rate,
                 applications[id].credit_amount,
                 applications[id].total_circle_limit,
-                applications[id].EMI,
-                applications[id].status);
+                applications[id].EMI
+                //applications[id].status
+                );
         
         //return applications[id]; (not working)
     }
     
-    function agreeForLoan(uint id) public {
+    function getCreditAmount(uint id) public view returns(uint){
+        return applications[id].credit_amount;
+    }
+    
+    function getlocker(address addr,uint id) public view returns (address) {
+        return borrwers[addr].circles[id].locker;
+    }
+    
+    function agreeForLoan(address addr,uint id) public {
+        // code 
+       
         
     }
     
-    
-    function checkagree(address addr) public view returns(uint, address, bool){
-        
-       return (participantslist[addr].id, participantslist[addr].participantsno, participantslist[addr].agree);
-    
+    function changestatusApproved(address addr,uint id) public {
+        applications[id].status = Status.Approved;
+        borrwers[addr].circles[id].investor = msg.sender;
         
     }
-    
-    function ifAgreed(address addr, uint id) public {
-          
-          borrwers[addr].circles[id].participantslist[msg.sender].agree = true;
-          
+    function changestatusShortlisted(uint id) public {
+        applications[id].status = Status.Shortlisted;
     }
+    
+    function ifAgreed(uint id) public {
+        //code if 80 % of participants agreed for loan
+        //uint members = borrwers[addr].circles[id].participants.length;
+        
+        applications[id].status = Status.Progress;
+    }
+    
+    function releaseLoan(address addr,uint id,uint appId) public {
+        Accounts acc = Accounts(AccountsContract);
+        //balances[applications[appId].borrower] += applications[appId].credit_amount * 40/100;
+        uint initialamount = applications[appId].credit_amount * 40/100;
+        acc.transfer(borrwers[addr].circles[id].locker,borrwers[addr].circles[id].borrower,initialamount);
+
+        // Populate loan object
+        loans[numLoans] = Loan(true, numLoans, borrwers[addr].circles[id].borrower, borrwers[addr].circles[id].investor, applications[appId].interest_rate, applications[appId].duration,
+        applications[appId].credit_amount, applications[appId].credit_amount, 0, block.timestamp,0, appId);
+        numLoans += 1;
+
+        //applications[appId].openApp = false;
+        hasOngoingLoan[borrwers[addr].circles[id].borrower] = true;
+    }
+    function PaytoInvestor(address addr,uint id,uint EMI) public payable {
+        address payer = borrwers[addr].circles[id].locker;
+        Accounts acc = Accounts(AccountsContract);
+        acc.transfer(payer,borrwers[addr].circles[id].investor,EMI);
+    }
+
+
+    function getinvestor(address addr,uint id) public view  returns (address)
+    {
+        return borrwers[addr].circles[id].investor;
+    }
+
 }
