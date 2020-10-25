6371117573dfe94001d6a6063583049d0e309af7
Akshaya G R <37238867+akshayacode@users.noreply.github.com>
2020-10-25 10:24:10 +0530

    create circle ,Trnasfer circle limit to locker

---
+++ Circles/Savings.sol
@@ -0,0 +1,142 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+import './Accounts.sol';
+contract Save{
+   
+    
+    // mapping (address => uint) balance;
+    
+
+    string FundName;
+    uint circleLimit;
+    uint targetamount;
+    uint installmentAmount;
+    uint noOfInstallments;
+    uint noOfparticipants;
+    address manager;
+    //address locker = 0xdD870fA1b7C4700F2BD7f44238821C26f7392148;
+    
+    mapping(address => bool) public participants;
+    uint public noOfParticipantsJoined;
+    bool public status = true;
+    address[] public participantsArray;
+    address[] public participantsToBePaid;
+    
+    mapping(address => uint) public contributedParticipants;
+    uint public currentInstallment = 1;
+    uint public currentNoOfContributors;
+    uint public fundBalance;
+    
+    //For bidding
+    uint  public LowestBid;
+    address public LowestBidder;
+    event LowestBidDecreased(address bidder, uint amount);
+    address  public defaultAddress ;
+    address public AccountsContract;
+    
+    //Locker
+  
+    function setAccountsContract(address addr) public{
+         AccountsContract = addr;
+    } 
+ 
+    function SavingCircle(string memory name,uint _circleLimit,uint _targetamount,uint _installmentAmount,uint _noOfInstallments,uint _noOfparticipants) public
+    {
+        FundName = name;
+        circleLimit = _circleLimit;
+        manager = msg.sender;
+        defaultAddress = address(uint160(uint(keccak256(abi.encodePacked(block.timestamp)))));
+        targetamount = _targetamount;
+        installmentAmount = _installmentAmount;
+        noOfInstallments = _noOfInstallments;
+        noOfparticipants = _noOfparticipants;
+        LowestBid = _targetamount;
+        Accounts acc = Accounts(AccountsContract);
+        acc.transfer(msg.sender,defaultAddress,circleLimit);
+        
+    }
+    // function CreateChitFund(uint _targetamount,uint _installmentAmount,uint _noOfInstallments,uint _noOfparticipants) public
+    // {
+       
+    //   targetamount = _targetamount;
+    //     installmentAmount = _installmentAmount;
+    //     noOfInstallments = _noOfInstallments;
+    //     noOfparticipants = _noOfparticipants;
+    //     LowestBid = _targetamount;
+   
+         
+    // }
+    
+    function joinFund() public {
+        require(noOfParticipantsJoined < noOfparticipants);
+        require(participants[msg.sender] != true);
+        require(status);
+
+        participants[msg.sender] = true;
+        participantsArray.push(msg.sender);
+        participantsToBePaid.push(msg.sender);
+        noOfParticipantsJoined++;
+    }
+     modifier isParticipant() {
+        require(participants[msg.sender] == true);
+        _;
+    }
+    modifier isManager() {
+        require(msg.sender == manager);
+        _;
+    }
+    
+    function contribute(uint amount) public isParticipant payable {
+        require(amount == installmentAmount);
+        require(contributedParticipants[msg.sender] != currentInstallment);
+        require(status);
+
+        fundBalance += amount;
+        Accounts ac = Accounts(AccountsContract);
+        ac.transfer(msg.sender,defaultAddress,amount);
+        currentNoOfContributors ++;
+        contributedParticipants[msg.sender] = currentInstallment;
+    }
+    
+    function participantsToBePaidLength() public view returns(uint) {
+        return participantsToBePaid.length;
+    }
+    function bid(uint amount) public payable {
+  
+        require(
+            amount < LowestBid,
+            "There already is a Lowest bid."
+        );
+
+        
+        LowestBidder = msg.sender;
+        LowestBid = amount;
+        emit LowestBidDecreased(msg.sender, amount);
+    }
+    
+    function releaseFund() public payable isManager {
+        Accounts acc = Accounts(AccountsContract);
+        require(currentNoOfContributors == noOfparticipants);
+        require(status);
+        if(currentInstallment == 1)
+        {
+            //balance[manager] += targetamount;
+            acc.transfer(defaultAddress,manager,targetamount);
+        }
+        else{
+        //balance[LowestBidder] += LowestBid;
+        acc.transfer(defaultAddress,LowestBidder,LowestBid);
+        }
+        currentInstallment ++;
+        fundBalance = targetamount - LowestBid;
+        currentNoOfContributors = 0;
+        LowestBid = targetamount;
+
+        if(currentInstallment == noOfInstallments) {
+            status = false;
+        }
+    }
+
+    
+}
