c041271053f26f765e6a75cb0c19e2bab4541f83
Akshaya G R <37238867+akshayacode@users.noreply.github.com>
2020-10-26 17:53:00 +0530

    View Status of application in viewapplicationdata

--- Circles/Investor.sol
+++ Circles/Investor.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.7.0;
 
 import "./Accounts.sol";
-import './Borrower.sol'; // import borrower. sol
+import './Circle.sol'; // import borrower. sol
 contract InvestorContract {
  
     mapping (address => Investor) public investors;
@@ -12,7 +12,6 @@ contract InvestorContract {
     address public contractaddr;
 
     function setcontractaddr(address borrower, address account) public   {
-      //BorrowerContractAddress = borrower;
       contractaddr = borrower;
       AccountsContractAddress = account;
    
@@ -35,24 +34,25 @@ contract InvestorContract {
     }
    
     function createInvestor(string memory name) public {
-        Investor memory investor;
-        investor.name = name;
-        investor.investor_public_key = msg.sender;
-        investor.EXISTS = true;
-        investors[msg.sender] = investor;
+        //Investor memory investor;
+        investors[msg.sender].name = name;
+        investors[msg.sender].investor_public_key = msg.sender;
+        investors[msg.sender].EXISTS = true;
+        //investors[msg.sender] = investor;
         hasOngoingInvestment[msg.sender] = false;
 
     }
   
 
-    function applicationdata(uint id) public view  returns(uint,uint,uint,uint,uint,uint) {
+    function applicationdata(uint id) public view  returns(uint,uint,uint,uint,uint,uint,Status) {
         CirclesContract cbwr = CirclesContract(contractaddr);
         return cbwr.viewApplication(id);
     }
     
+    // Using Javascript shortlist the applications
+    
     function Shortlist(uint id,uint interest_rate) public{
         CirclesContract cbwr = CirclesContract(contractaddr);
-        //cbwr.applications[id].interest_rate = interest_rate;
         investors[msg.sender].interestrate = interest_rate;
         investors[msg.sender].status = InvestorStatus.Shortlisted;
         cbwr.changestatusShortlisted(id);
@@ -61,16 +61,13 @@ contract InvestorContract {
    
     function grantloan(address addr,uint index) public payable {
         Accounts acc = Accounts(AccountsContractAddress);
-        //BorrowerContract bwr = BorrowerContract(BorrowerContractAddress);
         CirclesContract cbwr = CirclesContract(contractaddr);
         uint amount = cbwr.getCreditAmount(index);
         address beneficiary = cbwr.getlocker(addr,index);
-        require(acc.viewBalance(msg.sender) >= amount);
         require(hasOngoingInvestment[msg.sender] == false);
-        //address circlelocker = acc.locker();
         acc.transfer(msg.sender,beneficiary,amount);
         hasOngoingInvestment[msg.sender] = true;
-        cbwr.changestatusApproved(addr,index);
+        //cbwr.changestatusApproved(addr,index);
         investors[msg.sender].status = InvestorStatus.Debited;
     }
 
