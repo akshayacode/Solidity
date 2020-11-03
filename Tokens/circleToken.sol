b18a1be265a0b4f92a2b39f1def714ce97c3e5cc
Akshaya G R <37238867+akshayacode@users.noreply.github.com>
2020-11-03 11:35:18 +0530

    Update circleToken.sol

--- Tokens/circleToken.sol
+++ Tokens/circleToken.sol
@@ -10,9 +10,7 @@ library SafeMath {
     }
 
     function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        // assert(b > 0); // Solidity automatically throws when dividing by 0
         uint256 c = a / b;
-        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
         return c;
     }
 
@@ -47,11 +45,7 @@ contract StandardToken is Token {
     mapping(address => mapping (address => uint256)) internal allowed;
     mapping(address => uint256) balances;
 
-    /**
-    * @dev transfer token for a specified address
-    * @param _to The address to transfer to.
-    * @param _value The amount to be transferred.
-    */
+   
     function transfer(address _to, uint256 _value) public override returns (bool) {
         require(_to != address(0));
         require(balances[msg.sender] >= _value && balances[_to].add(_value) >= balances[_to]);
@@ -69,12 +63,6 @@ contract StandardToken is Token {
     }
 
 
-    /**
-    * @dev Transfer tokens from one address to another
-    * @param _from address The address which you want to send tokens from
-    * @param _to address The address which you want to transfer to
-    * @param _value uint256 the amount of tokens to be transferred
-    */
     function transferFrom(address _from, address _to, uint256 _value) public override returns (bool) {
         require(_to != address(0));
         require(_value <= allowed[_from][msg.sender]);
@@ -87,16 +75,8 @@ contract StandardToken is Token {
         return true;
     }
 
-    /**
-    * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
-    *
-    * Beware that changing an allowance with this method brings the risk that someone may use both the old
-    * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
-    * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
-    * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-    * @param _spender The address which will spend the funds.
-    * @param _value The amount of tokens to be spent.
-    */
+  
+    
     function approve(address _spender, uint256 _value) public override returns (bool) {
         allowed[msg.sender][_spender] = _value;
 
@@ -109,12 +89,7 @@ contract StandardToken is Token {
         return allowed[_owner][_spender];
     }
 
-    /**
-    * approve should be called when allowed[_spender] == 0. To increment
-    * allowed value is better to use this function to avoid 2 calls (and wait until
-    * the first transaction is mined)
-    * From MonolithDAO Token.sol
-    */
+  
     function increaseApproval(address _spender, uint256 _addedValue) public returns (bool success) {
         allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
 
@@ -136,11 +111,11 @@ contract StandardToken is Token {
     }
 }
 
-contract CircleContract is StandardToken {
-    string public constant name = "Circles Finance";
+contract CircleToken is StandardToken {
+    string public constant name = "Circles.Finance Token";
     string public constant symbol = "CIR";
     uint256 public constant decimals = 18;
-    uint256 public constant INITIAL_SUPPLY = 88888888 * (10**decimals);
+    uint256 public constant INITIAL_SUPPLY = 10000000 * (10**decimals);
     address public tokenWallet;
 
     constructor() {
